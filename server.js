53776b2a525eafbc557e929f6a48fe02b2c20ade
Mateo Pool <loopoetam@gmail.com>
2016-05-20 20:04:00 +0000

    Try number two, can I launch?

---
+++ server.js
@@ -0,0 +1,23 @@
+var http = require('http');
+var fs = require('fs');
+var map = require('through2-map');
+var url = require('url');
+var express = require('express');
+
+var server = http.createServer(function (req, res) {
+    var request = url.parse(req.url, true);
+    if (request.pathname == '' || request.pathname == '/') {
+        request.pathname = "/" + (Math.floor(Date.now() / 1000)).toString();
+    }
+    var date = request.pathname.slice(1).split("%20").join(' ');
+    var unixtime = Date.parse(date)/1000;
+    if(!isNaN(date)) {
+        unixtime = date;
+        date = new Date(Number(date) * 1000);
+        date = date.toString().slice(0, 15);
+    }
+    res.end(JSON.stringify({'unixtime': unixtime, "Natural": date}))
+  })
+
+
+server.listen(process.env.PORT)
